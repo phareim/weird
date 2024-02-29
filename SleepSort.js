06a74105bf149e97b924a44cac112264d664c810
Petter Hareim <petter.hareim@hkraft.no>
2024-02-29 22:49:43 +0100

    😴😴😴

---
+++ SleepSort.js
@@ -0,0 +1,14 @@
+function sleepSort(arr) {
+    arr.forEach(value => {
+        // Setter en timeout basert på verdien
+        setTimeout(() => {
+            console.log(value);
+        }, value * 100); // Multiplikasjonsfaktoren kan justeres
+    });
+}
+
+// Eksempel på bruk
+let arr = [3, 1, 4, 1, 5, 9, 2, 6];
+console.log("Original Array: ", arr);
+console.log("Sorted Array: ");
+sleepSort(arr);
