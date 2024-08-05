2c111207bc1dce61a58b4ffca7f73e34f2da3f69
Petter Hareim <petter.hareim@miles.no>
2024-08-05 14:06:08 +0200

    colors..

--- linked-list.js
+++ linked-list.js
@@ -6,12 +6,18 @@ function createList(count) {
     return list;
 }
 
-function printList(list) {
+function printList(list, name = "LIST", count = 10) {
+    console.log('\x1b[38;2;0;255;0m',`███ ${name}:`);
     var current = list;
+    var index = 0;
     while (current) {
-        console.log(current.value);
+        const gradient = Math.floor((index / count) * 255); // Adjust the divisor for more or fewer steps
+        const colorCode = `\x1b[38;2;0;${gradient};0m`;
+        console.log(`${colorCode}%s\x1b[0m`, `█ ${current.value}`);
         current = current.next;
+        index++;
     }
+    console.log('\n');
 }
 
 function reverse(node, reversePointer = null){
@@ -27,7 +33,5 @@ function reverse(node, reversePointer = null){
 let count = process.argv[2] || 10;
 let l = createList(count);
 
-console.log("\n\nLIST:\n");
-printList(l);
-console.log("\n\nREVERSE:\n");
-printList(reverse(l));
+printList(l, "LIST", count);
+printList(reverse(l), "REVERSE", count);
