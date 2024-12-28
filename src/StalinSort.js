3c7c088b063bced12b621a986da0d01b809fa378
Petter Hareim <phareim@gmail.com>
2024-12-29 00:04:43 +0100

    Update StalinSort.js

--- src/StalinSort.js
+++ src/StalinSort.js
@@ -6,6 +6,8 @@ function stalinSort(arr) {
     for (let i = 1; i < arr.length; i++) {
         if (arr[i] >= sortedArr[sortedArr.length - 1]) {
             sortedArr.push(arr[i]);
+        }else {
+            console.log(arr[i] + " has been sorted. 🔫");
         }
     }
 
